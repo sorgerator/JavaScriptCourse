ff0c2b3f91ce31a57707e115403109322e67aa3d
sorgerator <tarik.sorguc1@gmail.com>
2026-07-22 00:59:14 +0200

    Section 13

---
+++ 13-DOM and Browser API/adv-dom-sizes-html/assets/scripts/analytics.js
@@ -0,0 +1,10 @@
+const intervalId = setInterval(
+  () => {
+    console.log("Sending analytics data...");
+  },
+  2000,
+  [],
+);
+
+document.getElementById("stop-analytics-btn").addEventListener("click", () => clearInterval(intervalId);
+})
