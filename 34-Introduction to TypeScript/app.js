d1f95dc8b011a340273c12abf3b73c6887cadf0b
sorgerator <tarik.sorguc1@gmail.com>
2026-08-18 01:57:04 -0400

    Submitting Section 34

---
+++ 34-Introduction to TypeScript/app.js
@@ -0,0 +1,78 @@
+"use strict";
+/*
+class User {
+  name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+*/
+class User {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    print() {
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    permissions;
+    constructor(name, age, permissions) {
+        super(name, age);
+        this.permissions = permissions;
+    }
+}
+const user = new User("Max", 30);
+console.log(user.name);
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const buttonElement = document.querySelector("button");
+function add(a, b) {
+    return a + b;
+}
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+    else if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }
+    // console.log(result);
+}
+// type CalculationResult = { res: number; print: () => void }[];
+// let results: CalculationResult = [];
+const result = [];
+const names = ["Max"];
+buttonElement.addEventListener("click", () => {
+    const num1 = +num1Input.value;
+    const num2 = +num2Input.value;
+    const result = add(num1, num2);
+    const resultContainer = {
+        res: result,
+        print() {
+            console.log(this.res);
+        },
+    };
+    results.push(resultContainer);
+    // results.push(5);
+    // printResult(results);
+    //results[0].print();
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
+});
+function logAndEcho(val) {
+    console.log(val);
+    return val;
+}
+logAndEcho("Hi there!").spilt(" ");
